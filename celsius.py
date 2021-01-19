79f9283eb15baafece3d310fbd74b3776812378d
Steve <60276652+stevenwortmann@users.noreply.github.com>
2021-01-19 10:33:50 -0500

    Add files via upload

---
+++ celsius.py
@@ -0,0 +1,7 @@
+
+# Range of whole numbers 0-20
+# Formats string with values via modulo operator
+# Prints all rows evenly
+print("\n")
+for fah in range (0,21):
+    print("Celsius: %.2d C   Fahrenheit: %.1f F" % (fah, 1.8*fah + 32))
